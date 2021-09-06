e2ac2539ff7cbdc47f7a1664a9ec9bbb840d9f3a
Khel Austria <dev.geraldmichael@gmail.com>
2021-09-06 22:31:45 +0800

    added nav

---
+++ src/layout/MainLayout.vue
@@ -0,0 +1,46 @@
+<template>
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="#">Navbar</a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav">
+                    <!-- <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">Home</a>
+                    </li> -->
+                    <router-link tag="li" to="/">
+                        <a class="nav-link" aria-current="page">Home</a>
+                    </router-link>
+                    <router-link tag="li" to="/contact">
+                        <a class="nav-link">Contact</a>
+                    </router-link>
+                    <li class="nav-item">
+                        <a class="nav-link" @click="goTo('contact')">Contact</a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
+
+        <main>
+             <router-view/>
+        </main>
+    </div>
+</template>
+
+<script>
+export default {
+    methods: {
+        goTo(link) {
+            this.$router.push({path: link});
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/views/Contact.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="container"> 
+      <h1>Contact Page</h1>
+      <h2>Phone</h2>
+      <p v-if="showNumber">{{ phoneNumber }}</p>
+      <button @mouseover="togglePhone">Call Us</button>
+      <h3>Emails</h3>
+      <!-- <span v-for="e in emails" :key="e.id">{{ e.value }}</span> -->
+      <span>{{ emails[0].value }}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'ContactPage',
+    data() {
+        return {
+            showNumber: true,
+            phoneNumber: '09090909',
+            emails: [
+                {
+                    id: 1,
+                    value: 'gerald@gmail.com'
+                },
+                {
+                    id: 2,
+                    value: 'michael@gmail.com'
+                }
+            ]
+        }
+    },
+    // created() {
+    //     console.log("created");
+    // },
+    // beforeMount() {
+    //     this.initializePage();
+    // },
+    mounted() {
+        // this.initializePage();
+    },
+    methods: {
+        initializePage() {
+            alert("page has been initialized" + this.phoneNumber);
+        },
+        togglePhone() {
+            this.showNumber = !this.showNumber;
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>
